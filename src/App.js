327b8b0ab7912acef82f6c074d9cc0349ac59acd
Lucas Araujo <lukaasedu@gmail.com>
2020-09-10 16:49:57 -0300

    fix inputs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Form from './components/Form';
+import Installment from './components/Installment';
+import Installments from './components/Installments';
 
 export default function App() {
+  //Definição dos 4 estados com hooks
   const [initialInvestment, setInitialInvestment] = useState(1000);
   const [monthlyPeriod, setmonthlyPeriod] = useState(1);
   const [rateInterest, setRateInterest] = useState(1);
   const [installments, setInstallments] = useState([]);
+
+  //Monitoramento dos inputs
   useEffect(() => {
     calculateInstallments(initialInvestment, monthlyPeriod, rateInterest);
   }, [initialInvestment, monthlyPeriod, rateInterest]);
 
+  //Calculo do juros e Criação das parcelas
   const calculateInstallments = (
     initialInvestment,
     monthlyPeriod,
@@ -21,7 +27,7 @@ export default function App() {
     let percentage = 0;
 
     for (let i = 1; i <= monthlyPeriod; i++) {
-      const percentValue = (currentValue * rateInterest) / 100;
+      const percentValue = (currentValue * Math.abs(rateInterest)) / 100;
 
       currentValue =
         rateInterest >= 0
@@ -41,39 +47,43 @@ export default function App() {
     setInstallments(newInstallments);
   };
 
+  //Monitorar a mudança de dados
+  const handleChangeData = (newValue, newInterest, newPeriod) => {
+    if (newValue !== null) {
+      setInitialInvestment(newValue);
+      return;
+    }
+
+    if (newInterest !== null) {
+      setRateInterest(newInterest);
+      return;
+    }
+    setmonthlyPeriod(newPeriod);
+  };
+
   return (
     <div className="container">
       <h1 className="center">React Compound Interest</h1>
-      <div style={styles.nameContainerStyle}>
-        <Form
-          value={initialInvestment}
-          id={'inputInitalInvestment'}
-          label={'Initial Investment:'}
-          onChange={(value) => setInitialInvestment(value)}
-        />
-        <Form
-          value={rateInterest}
-          id={'inputRateInterest'}
-          label={'Rate Interest:'}
-          onChange={(value) => setRateInterest(value)}
-        />
-        <Form
-          value={monthlyPeriod}
-          id={'inputLenghtOfTime'}
-          label={'Lenght Of Time:'}
-          onChange={(value) => setmonthlyPeriod(value)}
-        />
-      </div>
+
+      <Form
+        data={{ initialInvestment, rateInterest, monthlyPeriod }}
+        onChangeData={handleChangeData}
+      />
+
+      <Installments>
+        {installments.map(({ id, value, difference, proffit, percentage }) => {
+          return (
+            <Installment
+              key={id}
+              id={id}
+              value={value}
+              difference={difference}
+              proffit={proffit}
+              percentage={percentage}
+            />
+          );
+        })}
+      </Installments>
     </div>
   );
 }
-
-const styles = {
-  nameContainerStyle: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: '5px',
-    marginLeft: '10px',
-  },
-};
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,57 @@
 import React from 'react';
 
-export default function Form({ value, onChange, id, label }) {
-  const handleChange = (event) => {
-    onChange(+event.target.value);
+export default function Form({ data, onChangeData }) {
+  const { initialInvestment, rateInterest, monthlyPeriod } = data;
+
+  const handleChangeInitialInvestment = (event) => {
+    onChangeData(+event.target.value, null, null);
+  };
+  const handleChangeRateInterest = (event) => {
+    onChangeData(null, +event.target.value, null);
+  };
+  const handleChangeMonthlyPeriod = (event) => {
+    onChangeData(null, null, +event.target.value);
   };
   return (
-    <div className="input-field">
-      <input id={id} type="number" value={value} onChange={handleChange} />
-      <label htmlFor="{id}" className="active">
-        {label}
-      </label>
+    <div className="center row">
+      <div className="col input-field s6 m4 l3">
+        <input
+          id="inputInitialInvestiment"
+          type="number"
+          value={initialInvestment}
+          onChange={handleChangeInitialInvestment}
+          min="100"
+          step="100"
+        />
+        <label htmlFor="inputInitialInvestment" className="active">
+          Initial Investment:
+        </label>
+      </div>
+      <div className="col input-field s6 m4 l3">
+        <input
+          id="inputRateInterest"
+          type="number"
+          value={rateInterest}
+          onChange={handleChangeRateInterest}
+          step="0.1"
+        />
+        <label htmlFor="inputRateInterest" className="active">
+          Rate Interest:
+        </label>
+      </div>
+      <div className="col input-field s6 m4 l3">
+        <input
+          id="inputMonthlyPeriod"
+          type="number"
+          value={monthlyPeriod}
+          onChange={handleChangeMonthlyPeriod}
+          min="1"
+          step="1"
+        />
+        <label htmlFor="inputMonthlyPeriod" className="active">
+          Monthly Period:
+        </label>
+      </div>
     </div>
   );
 }
